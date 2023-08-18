94a4e8e866410bb337410f9975edc1ab36391f6f
Mayank Shingala <142205731+mayankshingala@users.noreply.github.com>
2023-08-18 23:29:58 +0530

    Add files via upload

---
+++ Momentum_theory_result_for_the_Inflow_ratio/HT_AS_01_lambdavsmu_fpi.m
@@ -0,0 +1,34 @@
+%% Calculation of inflow ratio at the rotor disk in forward flight from the momentum theory and Solution of λ using Fixed Point Iteration compared with the approximate (valid for µ > 0.2) solution of λ
+
+clear all
+close all
+clc
+
+% Given Data
+alpha = 2*pi/180;               % Disk Angle of Attack (α)
+Ct = 0.006;                     % Thrust coefficient
+
+%% Fixed Point Iteration
+lambda_0 = sqrt(Ct/2);          % Initial guess of inflow ratio (typically hover inflow ratio)
+lambda(1) = lambda_0;
+
+for i = 1:1:301
+    mu(i) = 0.002*i - 0.002;                                                % 0<=µ<=0.6 ~~ varying Advanced ratio (Incompressible Subsonic regime)
+    lambda_apprx(i) = mu(i)*tan(alpha) + Ct/(2*mu(i));
+    lambda(i+1) = mu(i)*tan(alpha) + Ct/(2*sqrt(mu(i)^2 + lambda(i)^2));
+    lambda_fpi(i) = lambda(i+1);
+end
+
+% Plotting of data (Fixed Point Iteration)
+plot(mu,lambda_fpi,'r')
+hold on
+plot(mu,lambda_apprx,'--k')
+xlim([0 0.6])
+ylim([0 0.08])
+title('µ v/s λ (Fixed Point Iteration & Approximate Solution)')
+xlabel('Advance Ratio, µ')
+ylabel('Inflow Ratio, λ')
+legend('Fixed Point Iteration','Approximate Solution')
+grid on
+hold off
+annotation('textbox', [0.55, 0.68, 0.34, 0.1], 'String', "Disk Angle of Attack (α) = 2°, Thrust coefficient (Ct) = 0.006")
